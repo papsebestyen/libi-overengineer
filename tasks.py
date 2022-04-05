41454efef34d44ec906a87aecd0fc1237cc54b93
papsebestyen <pap.sebestyen@gmail.com>
2022-04-05 21:56:31 +0200

    make data dir

--- tasks.py
+++ tasks.py
@@ -5,5 +5,6 @@ def download_data(cli):
     from src.download.get_data import get_data
     from pathlib import Path
     data_path = Path('data')
+    data_path.mkdir()
     print(data_path.exists())
     get_data(data_path)
