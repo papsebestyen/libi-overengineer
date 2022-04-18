05bf3d5bb062004099b853000d51409e6689e054
lilibenedikta <55950892+lilibenedikta@users.noreply.github.com>
2022-04-18 19:03:36 +0100

    mappák visszacsinálása + dropdown

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-
-IBM_S3_ENDPOINT = 'https://s3.eu-de.cloud-object-storage.appdomain.cloud'
-IBM_BUCKET_NAME = 'libi'
--- gunicorn_config.py
+++
@@ -1,4 +0,0 @@
-accesslog = "/var/log/gunicorn.access.log"
-errorlog = "/var/log/gunicorn.error.log"
-capture_output = True
-loglevel = "debug"
