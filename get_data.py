10b8876d16f9e2e5282138ba7e3bc68f42002aa6
lilibenedikta <55950892+lilibenedikta@users.noreply.github.com>
2022-04-18 16:10:13 +0100

    heroku commit folder changes

---
+++ get_data.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta
+from pathlib import Path
+
+from pytrends.request import TrendReq
+
+
+kw_list = ["gift", "mug", "diy"]
+
+
+def get_day_before(day=datetime.now(), delta_days=365):
+    return day - timedelta(days=delta_days)
+
+
+pytrend = TrendReq()
+pytrend.build_payload(kw_list, timeframe="today 12-m")
+
+
+def get_data(day: datetime, data_path: Path):
+    """ "Get data from Google Trends."""
+    past = get_day_before(day, delta_days=365)
+    df = pytrend.get_historical_interest(
+        kw_list,
+        year_start=past.year,
+        month_start=past.month,
+        day_start=past.day,
+        hour_start=past.hour,
+        year_end=day.year,
+        month_end=day.month,
+        day_end=day.day,
+        hour_end=day.hour,
+        cat=0,
+        geo="",
+        gprop="",
+        sleep=0,
+    )
+    df.to_parquet(data_path / f"{day:%Y-%m-%d}.parquet")
---
+++ gunicorn_config.py
@@ -0,0 +1,4 @@
+accesslog = "/var/log/gunicorn.access.log"
+errorlog = "/var/log/gunicorn.error.log"
+capture_output = True
+loglevel = "debug"
